dbeb84fd7edd3fbcc460a90bfcc9585eb860018e
Akhile Fanny <95649144+Akhile-fanny@users.noreply.github.com>
2022-05-02 00:29:04 +0100

    Update 2019 Bike share Analysis.sql

--- 2019 Bike share Analysis.sql
+++ 2019 Bike share Analysis.sql
@@ -2,17 +2,17 @@ USE portfolio
 
 ---CREATING TEMPORARY TABLE TO HOUSE ALL THE 4 QUARTERS DATA
 CREATE TABLE #Temp_Divvy_Trips_2019( trip_id FLOAT, 
-									start_time DATETIME,
-									end_time DATETIME,
-									bikeid FLOAT,
-									tripduration FLOAT,
-									from_station_id FLOAT, 
-									from_station_name NVARCHAR(255),
-									to_station_id FLOAT,
-									to_station_name NVARCHAR (255),
-									usertype NVARCHAR(255), 
-									gender NVARCHAR(255), 
-									birthyear FLOAT)
+				start_time DATETIME,
+				end_time DATETIME,
+				bikeid FLOAT,
+				tripduration FLOAT,
+				from_station_id FLOAT, 
+				from_station_name NVARCHAR(255),
+				to_station_id FLOAT,
+				to_station_name NVARCHAR (255),
+				usertype NVARCHAR(255), 
+				gender NVARCHAR(255), 
+				birthyear FLOAT)
 
 
 --INSERTING DATA INTO THE TEMP TABLE
@@ -26,16 +26,7 @@ SELECT * FROM Divvy_Trips_2019_Q1
 	SELECT * FROM Divvy_Trips_2019_Q4
 
 
-
-	
-
-
-
 SELECT TOP 2 * FROM #Temp_Divvy_Trips_2019
-
-
-
-
 
 
 	--- MONTHS BY MONTHLY USERS
@@ -46,27 +37,10 @@ SELECT TOP 2 * FROM #Temp_Divvy_Trips_2019
 	ORDER BY Monthly_users DESC
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 	-- WHO ARE THE USER TYPE
 	SELECT  DISTINCT usertype
 	FROM #Temp_Divvy_Trips_2019
 
-
-	
 	--HOW MANY CUSTOMERS RIDE ON A MONTHLY BASIS?
 	SELECT DISTINCT( DATENAME(Month, start_time)) AS MONTH,
 					COUNT(DATENAME(Month, start_time))
@@ -75,10 +49,6 @@ SELECT TOP 2 * FROM #Temp_Divvy_Trips_2019
 	WHERE start_time < end_time AND usertype = 'customer'
 	ORDER BY Monthly_Customer DESC
 
-
-
-
-
 	--HOW MANY CUSTOMERS RIDE ON A MONTHLY BASIS?
 	SELECT DISTINCT( DATENAME(Month, start_time)) AS MONTH, COUNT(DATENAME(Month, start_time))
 		OVER( PARTITION BY DATENAME(Month, start_time)) AS Monthly_Subscriber
@@ -86,28 +56,14 @@ SELECT TOP 2 * FROM #Temp_Divvy_Trips_2019
 	WHERE start_time < end_time AND usertype = 'subscriber'
 	ORDER BY Monthly_Subscriber DESC
 
-
-
-
-
-
-
 	--TOTAL BIKE USERS IN 2019
 	SELECT COUNT(trip_id) AS Total_bike_users
 	FROM #Temp_Divvy_Trips_2019
-
-	
-	
 	
 	-- TOTAL CUSTOMERS
 SELECT COUNT(trip_id) AS TotalCustomers
 	FROM #Temp_Divvy_Trips_2019
 	WHERE usertype = 'customer'
-
-
-
-
-
 
 -- TOTAL SUBSCRIBERS
 SELECT COUNT(trip_id) AS TotalSubscriber
@@ -116,8 +72,8 @@ SELECT COUNT(trip_id) AS TotalSubscriber
 
 
 	   --- AVERAGE TRIP DURATION IN MINUTES
-   	SELECT ROUND(AVG(tripduration/ 60), 0) AS Average_trip_duration
-	FROM #Temp_Divvy_Trips_2019
+SELECT ROUND(AVG(tripduration/ 60), 0) AS Average_trip_duration
+FROM #Temp_Divvy_Trips_2019
 
 
 	   -- count of gender 
@@ -136,14 +92,13 @@ FROM #Temp_Divvy_Trips_2019
 
    ---TEMP TABLE
    CREATE TABLE #Temp_weeklyRide( usertype NVARCHAR (255),
-									Weekday VARCHAR(20)) 
+				Weekday VARCHAR(20)) 
 
 
     --weekly ride
 	INSERT INTO #Temp_weeklyRide
  SELECT  usertype, DATENAME(WEEKDAY, start_TIME) AS Weekday
  FROM #Temp_Divvy_Trips_2019
-
 
 
  SELECT * FROM #Temp_weeklyRide
@@ -171,37 +126,11 @@ FROM #Temp_Divvy_Trips_2019
  GROUP BY  weekday
  ORDER BY SubscriberWeeklyRide DESC
 
-
   	---Minimum ride duration by usertype
 	SELECT Usertype,  MIN(CAST(end_time - start_time AS time)) AS Min_Ride_duration
    FROM #Temp_Divvy_Trips_2019
    WHERE start_time < end_time
    GROUP BY usertype
-
-   ----- CONFIRMATION
-  -- SELECT  CAST(end_time - start_time AS TIME)  AS Ride_duration,usertype,
-  -- end_time, start_time 
-  -- FROM #Temp_Divvy_Trips_2019
-  -- WHERE start_time < end_time
-  --ORDER BY Ride_duration
-    
-   
-   --SELECT end_time, start_time, CAST(end_time - start_time AS TIME)  AS Ride_duration
-   --FROM  #Temp_Divvy_Trips_2019
-			--WHERE start_time > end_time
-  
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     	---MAXIMUM RIDE DURATION BY USERTRYPE
@@ -212,29 +141,12 @@ FROM #Temp_Divvy_Trips_2019
    ORDER BY Max_Ride_duration DESC
 
 
-
-
-   ---USER RIDE DURATION 
-   --SELECT usertype, CAST(end_time - start_time AS TIME) AS ride_duration
-   --FROM #Temp_Divvy_Trips_2019
-   --ORDER BY ride_duration DESC
-
    ---USER RIDE DURATION 
    SELECT usertype,CAST(end_time - start_time AS TIME) AS ride_duration
    FROM #Temp_Divvy_Trips_2019
     WHERE start_time < end_time
    ORDER BY ride_duration DESC
 
-
-   -- SELECT usertype,CAST(end_time - start_time AS nvarchar (255)) AS ride_duration
-   --FROM #Temp_Divvy_Trips_2019
-   --ORDER BY ride_duration DESC
-
-
-   --SELECT DATENAME(YEAR, start_time) - birthyear AS Age
-   --FROM #Temp_Divvy_Trips_2019
-
- ---CREATING A TEMP TABLE
  
  CREATE TABLE #Temp_AgeOfRider ( usertype NVARCHAR(255), Age float, Ride_duration TIME)
 
@@ -243,11 +155,8 @@ FROM #Temp_Divvy_Trips_2019
 		DATENAME(YEAR, start_time) - birthyear AS Age,
 		CAST(end_time - start_time AS TIME) AS ride_duration
 		FROM #Temp_Divvy_Trips_2019
-	 WHERE birthyear >= 1939 AND birthyear IS NOT NULL AND birthyear <> 2014 AND start_time < end_time
-	 ORDER BY Age
-
-
-
+WHERE birthyear >= 1939 AND birthyear IS NOT NULL AND birthyear <> 2014 AND start_time < end_time
+ ORDER BY Age
 
 
 	 -- USERTYPE BY AGE RANGE
@@ -266,13 +175,13 @@ FROM #Temp_Divvy_Trips_2019
 
    --	AGE BY TRIP DURATION
    SELECT temp.age Age, ROUND(divvy.tripduration / 60,0) AS TripDuration 
-   FROM #Temp_AgeOfRider AS temp JOIN #Temp_Divvy_Trips_2019 AS Divvy ON temp.usertype = divvy.usertype
+   FROM #Temp_AgeOfRider AS temp JOIN 
+   				#Temp_Divvy_Trips_2019 AS Divvy ON temp.usertype = divvy.usertype
 
    --- WHAT IS THE AVEGERAGE AGEOF THE USER TYPE
 SELECT usertype, ROUND(AVG(Age), 0) AS Average_Age
  FROM #Temp_AgeOfRider
 GROUP BY usertype
-
 
    
    --- USERTYPE BY  MAX RIDE DURATION AND MAX  AGE  DURATION
@@ -300,10 +209,6 @@ GROUP BY usertype
 					ELSE NULL END 
 
 
-
-
-
-
 --- WHAT IS THE MONTHLY AVERAGE TRIP DURATION OF CUSTOMER IN MINUTES?
     SELECT  DATENAME(MONTH, start_time) AS Months, 
 			ROUND(AVG(tripduration) / 60,0) AS CustomerAvg_trip_duration
@@ -320,27 +225,3 @@ SELECT  DATENAME(MONTH, start_time) AS Months,
    WHERE usertype ='subscriber'
    GROUP BY   DATENAME(MONTH, start_time)
    ORDER BY subscriberAvg_trip_duration DESC 
-
-
-
-
-
-
-
-
-
-   --CREATING A TEMP TABLE FOR  WEEKDAY
-
---CREATE TABLE #Temp_week( Day_of_the_week varchar(10), Total_sales float, week_no int)
---INSERT INTO #Temp_week 
---SELECT  DISTINCT DATENAME(WEEKDAY, date) AS Day_of_the_week, Total_sales,
---				CASE WHEN DATENAME(WEEKDAY, date) = 'Monday' THEN 1
---						WHEN DATENAME(WEEKDAY, date) = 'Tuesday' THEN 2
---						WHEN DATENAME(WEEKDAY, date) = 'wednesday' THEN 3
---						WHEN DATENAME(WEEKDAY, date) = 'thursday' THEN 4
---						WHEN DATENAME(WEEKDAY, date) = 'friday' THEN 5
---						WHEN DATENAME(WEEKDAY, date) = 'saturday' THEN 6
---						WHEN DATENAME(WEEKDAY, date) = 'sunday' THEN 7
---						ELSE NULL END AS WEEK_no 		
---FROM TEMP_Transaction 
---GROUP BY DATENAME(WEEKDAY, date),Total_sales
